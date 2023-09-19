fa3c4de3029e3747af235958401fb592ec7523d1
ARS Talukder <talukderars@gmail.com>
2023-09-19 21:22:53 +0600

    Data types are end here

---
+++ Day-01-data-types/hex-oct.js
@@ -0,0 +1,11 @@
+const hex1 = 0x12;
+console.log(hex1); //18 (in decimal)
+
+const hex2 = 0xff;
+console.log(hex2); //255 (in decimal)
+
+const oct1 =0o12;
+console.log(oct1); //10 (in decimal)
+
+const oct2 =0o255;
+console.log(oct2); //173 (in decimal)
---
+++ Day-01-data-types/number-system-conversion.js
@@ -0,0 +1,8 @@
+const decNum = 4579;
+const hexStr = decNum.toString(16);
+console.log(hexStr); //11e3
+
+const octStr = decNum.toString(8);
+console.log(octStr); //10743
+
+
